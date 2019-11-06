fac2630c58718469e569754a2246905ed4506753
Jordan <jordan.pedro27@gmail.com>
2019-11-06 01:09:13 +0100

    Add sign in & sign up form front

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -1,8 +1,6 @@
 class EventController < ApplicationController
   before_action :authenticate_user, only: [:new, :create]
 
-   private
-
    def authenticate_user
      unless current_user
        flash[:danger] = "Connecte toi avant !"
@@ -12,6 +10,7 @@ class EventController < ApplicationController
 
 
   def index
+    @images = ['https://blackwhite.pictures/public/uploads/thumbnail/blackwhite-photography-36271549213698klqcoxfiqq.jpeg', 'https://www.letelegramme.fr/images/2019/10/17/le-bunker-brestois-grand-messe-de-la-scene-electro-local_4862281_495x330p.jpg?v=1', 'http://studio-spaces.com/wp-content/uploads/2016/09/2_StudioSpaces_Events_Wedding_03.jpg', 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTvz75lpxAuMhrm1NCV41VDDU00bIQ8N_rn8ky281kWcpoM2EgO', 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRcuaAyWCW4biOA-C8BQ9e_Pi73X53nCkamqz4oYUf_5FaloHCg', 'https://apiumhub.com/wp-content/uploads/2017/01/become-a-software-developer.jpg']
   end
 
   def show
